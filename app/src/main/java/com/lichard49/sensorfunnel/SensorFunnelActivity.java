4833dcd0f7be0222aca458579ea1ddf2d4e52bc5
richard <lichard49@gmail.com>
2016-08-14 13:54:58 -0400

    Base Android application

---
+++ app/src/main/java/com/lichard49/sensorfunnel/SensorFunnelActivity.java
@@ -0,0 +1,13 @@
+package com.lichard49.sensorfunnel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SensorFunnelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_funnel);
+    }
+}
