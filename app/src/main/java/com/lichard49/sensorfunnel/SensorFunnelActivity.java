0e9901d4ddfff1b9f2a3b6ac452638c1075b1f30
richard <lichard49@gmail.com>
2016-08-14 22:04:11 -0400

    Add worker thread to service that can be started/stopped from Activity

--- app/src/main/java/com/lichard49/sensorfunnel/SensorFunnelActivity.java
+++ app/src/main/java/com/lichard49/sensorfunnel/SensorFunnelActivity.java
@@ -1,8 +1,11 @@
 package com.lichard49.sensorfunnel;
 
 import android.content.Intent;
+import android.hardware.Sensor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SensorFunnelActivity extends AppCompatActivity {
 
@@ -13,5 +16,17 @@ public class SensorFunnelActivity extends AppCompatActivity {
 
         Intent startService = new Intent(SensorFunnelActivity.this, SensorFunnelService.class);
         startService(startService);
+
+        Button startButton = (Button) findViewById(R.id.button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(SensorFunnelService.getInstance().isReadingData()) {
+                    SensorFunnelService.getInstance().stopReadingData();
+                } else {
+                    SensorFunnelService.getInstance().startReadingData();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/lichard49/sensorfunnel/SensorFunnelService.java
+++ app/src/main/java/com/lichard49/sensorfunnel/SensorFunnelService.java
@@ -14,15 +14,41 @@ import android.util.Log;
  */
 public class SensorFunnelService extends Service {
 
+    private static SensorFunnelService serviceInstance;
+
     private final int NOTIFICATION_ID = 169;
 
     private NotificationManager notificationManager;
+    private Notification.Builder notificationBuilder;
+
+    private boolean isReadingData = false;
+    private Runnable readDataThread = new Runnable() {
+        @Override
+        public void run() {
+            // connect to sensors
+            int i = 0;
+
+            while(isReadingData) {
+                try {
+                    Thread.sleep(1000);
+                    updateNotificationContent(i + " finished");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                i++;
+            }
+
+            updateNotificationContent("Done");
+        }
+    };
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         Log.d("lichard49", "Service started!");
+        serviceInstance = this;
         showNotification();
     }
 
@@ -41,6 +67,10 @@ public class SensorFunnelService extends Service {
         return null;
     }
 
+    public static SensorFunnelService getInstance() {
+        return serviceInstance;
+    }
+
     private void showNotification() {
         // https://gist.github.com/kristopherjohnson/6211176
         Intent showTaskIntent = new Intent(getApplicationContext(), SensorFunnelActivity.class);
@@ -55,16 +85,37 @@ public class SensorFunnelService extends Service {
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        Notification serviceNotification  = new Notification.Builder(this)
+        notificationBuilder = new Notification.Builder(this)
                 .setContentTitle("Sensor Funnel")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setOngoing(true)
-                .setContentIntent(startActivity)
-                .build();
-        notificationManager.notify(NOTIFICATION_ID, serviceNotification);
+                .setContentIntent(startActivity);
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
     }
 
     private void hideNotification() {
         notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    private void updateNotificationContent(String content) {
+        notificationBuilder.setContentText(content);
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    public void startReadingData() {
+        if(!isReadingData) {
+            isReadingData = true;
+            new Thread(readDataThread).start();
+        }
+    }
+
+    public void stopReadingData() {
+        if(isReadingData) {
+            isReadingData = false;
+        }
+    }
+
+    public boolean isReadingData() {
+        return isReadingData;
     }
 }
